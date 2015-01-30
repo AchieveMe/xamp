e77e450be67509cdafefbf3e1197f023bb114383
Guilherme Magalhaes <guiandmag@gmail.com>
2015-01-30 10:18:54 -0200

    Initial of everything

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name xampApp
+ * @description
+ * # xampApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('xampApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/profile', {
+        templateUrl: 'views/profile.html',
+        controller: 'ProfileCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
