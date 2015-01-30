565ce2fddb7e6ed54084c9a677eb978aba77160c
Guilherme Magalhaes <guiandmag@gmail.com>
2015-01-30 15:16:15 -0200

    Alteracao no arquivo Gruntfile.js para deixar a adicao de bibliotecas manual, e alteracao de nome de classes

--- test/spec/controllers/profile.js
+++ test/spec/controllers/profile.js
@@ -1,17 +1,17 @@
 'use strict';
 
-describe('Controller: AboutCtrl', function () {
+describe('Controller: ProfileCtrl', function () {
 
   // load the controller's module
   beforeEach(module('xampApp'));
 
-  var AboutCtrl,
+  var ProfileCtrl,
     scope;
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
     scope = $rootScope.$new();
-    AboutCtrl = $controller('AboutCtrl', {
+    ProfileCtrl = $controller('ProfileCtrl', {
       $scope: scope
     });
   }));
